57902e010e1619c80ebc478dbc5892a8071a9983
Miguel Angel Mateo Collado <miguizindex@gmail.com>
2023-01-11 23:56:36 -0400

    prueba

--- api/routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-const getDrinks = require("./routes/getDrinks.js");
-const getCategory = require("./routes/getCategory.js");
-const getOrigen = require("./routes/getOrigen.js");
-router.use("/", getDrinks);
-router.use("/", getCategory);
-router.use("/", getOrigen);
-module.exports = router;
